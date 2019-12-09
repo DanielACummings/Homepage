82aea30c392d6f699170f5925b5231e59211d157
Daniel <danielac@protonmail.com>
2019-12-09 10:44:14 -0700

    displays in celcius instead of kelvin

--- app/models/weather.js
+++ app/models/weather.js
@@ -1,4 +1,8 @@
 export default class Weather {
+  kToC(k) {
+    let c = k - 273.15
+    return c.toFixed(1)
+  }
   constructor(data) {
     //NOTE Have you ever wanted to know the temperature measured in kelvin? 
     //      That is what this data returns! data.main.temp is the temperature in Kelvin
@@ -8,12 +12,12 @@ export default class Weather {
     //      check out the other data that comes back and see if there is anything you want to try
 
     this.city = data.name
-    this.kelvin = data.main.temp
+    this.kelvin = this.kToC(data.main.temp)
   }
   get weatherTemplate() {
     return /*html*/`
     <div>
-    <p>${this.kelvin}°K</p>
+    <p>${this.kelvin}°&nbspC</p>
     <small>${this.city}</small>
     </div>`
   }
